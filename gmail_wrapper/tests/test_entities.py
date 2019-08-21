ab025f0161110a8fb64378fcc761f0b38455e59e
Luiz Rosa <luiz.rosa@loadsmart.com>
2019-08-21 14:41:55 -0300

    TestAttachment

--- gmail_wrapper/tests/test_entities.py
+++ gmail_wrapper/tests/test_entities.py
@@ -1,6 +1,6 @@
 import datetime
 
-from gmail_wrapper.entities import Message, Attachment
+from gmail_wrapper.entities import Message, Attachment, AttachmentBody
 
 
 class TestMessage:
@@ -46,4 +46,33 @@ class TestMessage:
                 isinstance(attachment, Attachment) and attachment.filename
                 for attachment in complete_message.attachments
             ]
+        )
+
+
+class TestAttachment:
+    def test_it_has_basic_properties_without_additional_fetch(
+        self, mocker, client, raw_complete_message
+    ):
+        mocked_get_attachment_body = mocker.patch(
+            "gmail_wrapper.client.GmailClient.get_attachment_body"
+        )
+        raw_attachment = raw_complete_message["payload"]["parts"][1]
+        incomplete_attachment = Attachment("123AAB", client, raw_attachment)
+        assert incomplete_attachment.id == raw_attachment["body"]["attachmentId"]
+        assert incomplete_attachment.filename == raw_attachment["filename"]
+        mocked_get_attachment_body.assert_not_called()
+
+    def test_it_fetch_additional_information_when_needed(
+        self, mocker, client, raw_complete_message, raw_attachment_body
+    ):
+        mocked_get_attachment_body = mocker.patch(
+            "gmail_wrapper.client.GmailClient.get_attachment_body",
+            return_value=AttachmentBody(raw_attachment_body),
+        )
+        incomplete_attachment = Attachment(
+            "123AAB", client, raw_complete_message["payload"]["parts"][1]
+        )
+        assert incomplete_attachment.content == b"The Quick Brown Fox Jumps Over The Lazy Dog"
+        mocked_get_attachment_body.assert_called_once_with(
+            raw_attachment_body["attachmentId"], "123AAB"
         )
