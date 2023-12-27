4bcbd218922d0ff22005215268b8263bd4944cc3
yanhenning <yan.henning@loadsmart.com>
2023-12-27 11:39:22 -0300

    drop support for 3.6

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
     classifiers=[
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
